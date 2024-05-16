6be5451156e92de1f84bff2dbbff370a04704800
TobiasMastroberardini <tmastroberardini1@gmail.com>
2024-05-16 15:36:14 -0300

    cambios

--- js/componentes/oferta.js
+++ js/componentes/oferta.js
@@ -1,3 +1,5 @@
+import { cargarProductosHTML } from "./cargarProductos.js";
+
 // Función para cargar los productos en oferta en el HTML
 const cargarProductosEnOferta = function () {
     fetch('./datos/productos.json')
@@ -8,39 +10,7 @@ const cargarProductosEnOferta = function () {
         });
 }
 
-function cargarProductosHTML(datos) {
-    const productosContainer = document.querySelector('.articulos');
-
-    datos.forEach(function (producto) {
-        const productoElement = document.createElement('article');
-
-        const nombreElement = document.createElement('h3');
-        nombreElement.className = 'bold';
-        nombreElement.textContent = producto.nombre;
-
-        const imagenesContainer = document.createElement('div');
-        imagenesContainer.className = 'contImg';
-        producto.imagen.forEach(function (imagen) {
-            const imagenElement = document.createElement('img');
-            imagenElement.src = imagen.ubicacion;
-            imagenElement.alt = imagen.textoAlt;
-            imagenElement.className = 'imgProducto'; // Ajusta esta clase según tu necesidad
-            imagenesContainer.appendChild(imagenElement);
-        });
-
-        const descripcionElement = document.createElement('p');
-        descripcionElement.textContent = producto.descripcion;
-
-        const pesoElement = document.createElement('p');
-        pesoElement.textContent = `Peso: ${producto.peso}`;
-
-        const precioElement = document.createElement('p');
-        precioElement.textContent = `Precio: ${producto.precio}`;
-
-        productoElement.append(nombreElement, imagenesContainer, descripcionElement, pesoElement, precioElement);
-        productosContainer.appendChild(productoElement);
-    });
-}
+cargarProductosHTML();
 
 export { cargarProductosEnOferta };
 
