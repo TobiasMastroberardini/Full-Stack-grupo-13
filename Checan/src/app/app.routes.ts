ca7c666252155b13905b0f5ed3ed9d57cf4c09ef
TobiasMastroberardini <tmastroberardini1@gmail.com>
2024-06-05 00:36:04 -0300

    Adding components

--- Checan/src/app/app.routes.ts
+++ Checan/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +13,10 @@ export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent,
+    },
+    {
+        path: 'product/:url',
+        component: ProductComponent,
     },
     {
         path: '**',  // Ruta para manejar URLs no encontradas
