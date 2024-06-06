32f234b0aa6e950af851a1c9dd5a32323db5832d
TobiasMastroberardini <tmastroberardini1@gmail.com>
2024-06-06 14:38:23 -0300

    Many things

--- Checan/src/app/product-card/product-card.component.ts
+++ Checan/src/app/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
+import { AddToCartButtonComponent } from "../add-to-cart-button/add-to-cart-button.component";
 import { CartComponent } from "../cart/cart.component";
 import { ProductInfoComponent } from "../product-info/product-info.component";
 import { ViewProductButtonComponent } from "../view-product-button/view-product-button.component";
@@ -10,7 +11,7 @@ import { ViewProductButtonComponent } from "../view-product-button/view-product-
   standalone: true,
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
-  imports: [CommonModule, HttpClientModule, ProductInfoComponent, ViewProductButtonComponent, CartComponent]
+  imports: [CommonModule, HttpClientModule, ProductInfoComponent, ViewProductButtonComponent, CartComponent, AddToCartButtonComponent]
 })
 export class ProductCardComponent {
 
