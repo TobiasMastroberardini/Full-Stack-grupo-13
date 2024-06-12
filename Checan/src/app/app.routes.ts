d61038d7575d54f9e6186a6b42482bf353e5fbff
TobiasMastroberardini <tmastroberardini1@gmail.com>
2024-06-12 00:59:48 -0300

    new component log-in

--- Checan/src/app/app.routes.ts
+++ Checan/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
 import { NewsComponent } from './news/news.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductInfoComponent } from './product-info/product-info.component';
@@ -41,6 +42,10 @@ export const routes: Routes = [
     {
         path: 'contact',
         component: ContactComponent,
+    },
+    {
+        path: 'login',
+        component: LogInComponent,
     },
     {
         path: '**',  // Ruta para manejar URLs no encontradas
