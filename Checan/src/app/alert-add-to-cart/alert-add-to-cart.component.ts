36629b4d75722699b71dfa4ee2bfe1ed959a461f
TobiasMastroberardini <tmastroberardini1@gmail.com>
2024-06-12 00:15:08 -0300

    Big changes in style

--- Checan/src/app/alert-add-to-cart/alert-add-to-cart.component.ts
+++ Checan/src/app/alert-add-to-cart/alert-add-to-cart.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AlertService } from '../alert.service';
+import { CartStateService } from '../cart-state.service';
 
 @Component({
   selector: 'app-alert-add-to-cart',
@@ -13,8 +14,20 @@ import { AlertService } from '../alert.service';
 export class AlertAddToCartComponent implements OnInit, OnDestroy {
   message: string | null = null;
   private alertSubscription: Subscription | undefined;
+  isCartOpen: boolean = false;
 
-  constructor(private alertService: AlertService) { }
+  constructor(private alertService: AlertService, private cartStateService: CartStateService) {
+    this.isCartOpen = false;
+  }
+
+  toggleCartService() {
+    this.cartStateService.toggleCart();
+  }
+
+  toggleCart() {
+    this.isCartOpen = !this.isCartOpen;
+  }
+
 
   ngOnInit() {
     this.alertSubscription = this.alertService.alert$.subscribe(message => {
--- Checan/src/app/app.component.ts
+++ Checan/src/app/app.component.ts
@@ -5,14 +5,13 @@ import { AlertAddToCartComponent } from "./alert-add-to-cart/alert-add-to-cart.c
 import { CartComponent } from "./cart/cart.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
-import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, HttpClientModule, HeaderComponent, NavbarComponent, FooterComponent, CartComponent, AlertAddToCartComponent]
+  imports: [RouterOutlet, HttpClientModule, HeaderComponent, FooterComponent, CartComponent, AlertAddToCartComponent]
 })
 export class AppComponent {
   title = 'Checan';
